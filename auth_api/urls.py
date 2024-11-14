dcb504e4b268f3d20665bbda5cedd6cc630086d1
DavidDebrah <debrahdavid1@gmail.com>
2024-11-14 04:07:25 +0000

    modified

--- auth_api/urls.py
+++ auth_api/urls.py
@@ -5,9 +5,9 @@ urlpatterns = [
     path('signup', views.signup, name='signup'),
     path('signin', views.signin, name='signin'),
     path('get_user', views.get_user, name='get_user'),
-    # path('create-task', views.createTask, name='create-task'),
-    # path('tasks', views.tasks, name='tasks'),
-    # path('delete-task/<str:pk>', views.deleteTask, name='deleteTask'),
-    # path('complete-task/<str:pk>', views.completeTask, name='completeTask'),
+    path('create-task', views.createTask, name='create-task'),
+    path('tasks', views.tasks, name='tasks'),
+    path('delete-task/<str:pk>', views.deleteTask, name='deleteTask'),
+    path('complete-task/<str:pk>', views.completeTask, name='completeTask'),
 
 ]
