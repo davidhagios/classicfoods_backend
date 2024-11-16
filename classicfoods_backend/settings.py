73b07a7f0a2596512a05681572c788b476bdef1f
DavidDebrah <debrahdavid1@gmail.com>
2024-11-16 12:41:43 +0000

    modified

--- classicfoods_backend/settings.py
+++ classicfoods_backend/settings.py
@@ -119,8 +119,13 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = 'media/'
+
+MEDIAFILES_DIRS = [
+    os.path.join(BASE_DIR, 'media'),
+]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
 
 # Default primary key field type
